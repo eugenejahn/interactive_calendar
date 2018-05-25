fbc80e457b146fdd19e87e33065c3a82c6ece37e
Antonia <asvenmosalay@hotmail.com>
2018-05-25 12:01:54 -0700

    Almost done with the week calendar

--- javaapplication1/CldPanel.java
+++ javaapplication1/CldPanel.java
@@ -10,16 +10,26 @@ public class CldPanel extends JPanel{
    public void paintComponent (Graphics g) {
       super.paintComponent(g);
       g.setColor(Color.BLACK);
-      for(int i = 0; i < 7; i++) {
+      g.drawLine(0,0,0,1000);
+      for(int i = 1; i <= 7; i++) {
           g.drawRect(i*100,0,100,1000);
       }
-      g.drawLine(0,20,1000,20);
-      g.drawString("Monday",0,15);
-      g.drawString("Tuesday",100,15);
-      g.drawString("Wednesday",200,15);
-      g.drawString("Thursday",300,15);
-      g.drawString("Friday",400,15);
-      g.drawString("Saturday",500,15);
-      g.drawString("Sunday",600,15);      
+      for(int j = 1; j <= 12; j++) {
+         g.drawString ("" + j + "am",10,40*j);
+      }
+      for(int j = 13; j <= 24; j++) {
+         g.drawString ("" + j + "pm",10,40*j);
+      }
+      for(int k = 0; k < 24; k++) {
+         g.drawLine(0,k*40 + 20,800,k*40 + 20);
+      }
+      g.drawLine(0,20,800,20);
+      g.drawString("Monday",110,15);
+      g.drawString("Tuesday",210,15);
+      g.drawString("Wednesday",310,15);
+      g.drawString("Thursday",410,15);
+      g.drawString("Friday",510,15);
+      g.drawString("Saturday",610,15);
+      g.drawString("Sunday",710,15);      
    }
 }
